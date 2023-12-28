11976eaa03b79405c4eefe64edf05ecd33d6a481
neleulvi <ulvinele@gmail.com>
2023-12-28 11:03:24 +0200

    Timetracking

--- cypress/e2e/issue-time-tracking.cy.js
+++ cypress/e2e/issue-time-tracking.cy.js
@@ -12,6 +12,7 @@ describe("Time tracking functionality", () => {
   const getIssueDetailsModal = () =>
     cy.get('[data-testid="modal:issue-details"]');
   const getTimeTrackingModal = () => cy.get('[data-testid="modal:tracking"]');
+  const Stopwatch = '[data-testid="icon:stopwatch"]';
 
   it("Should add, edit and delete estimation time", () => {
     //Adding estimation.
@@ -35,38 +36,49 @@ describe("Time tracking functionality", () => {
     });
   });
 
-  it.only("Should log, edit and delete logged time", () => {
-    getIssueDetailsModal().within(() => {
-      //Log time.
-      cy.get('[data-testid="icon:stopwatch"]').click();
-    });
-
+  it("Should log, edit and delete logged time", () => {
+    //Log time.
+    cy.get(Stopwatch).click();
     getTimeTrackingModal().within(() => {
+      cy.contains("Time spent (hours)");
       cy.get('[placeholder="Number"]').first().click().clear().type("8");
+      cy.contains("Time remaining (hours)");
+      cy.get('[placeholder="Number"]').last().click().clear().type("0");
       cy.contains("button", "Done").click().should("not.exist");
     });
 
     //Asserting that the logged time is added and visible.
     getIssueDetailsModal().within(() => {
       cy.contains("div", "8h logged").should("exist");
+      cy.contains("div", "0h remaining").should("exist");
     });
     //Editing the logged time.
+    cy.get(Stopwatch).click();
     getTimeTrackingModal().within(() => {
+      cy.contains("Time spent (hours)");
       cy.get('[placeholder="Number"]').first().click().clear().type("6");
+      cy.contains("Time remaining (hours)");
+      cy.get('[placeholder="Number"]').last().click().clear().type("2");
       cy.contains("button", "Done").click().should("not.exist");
     });
     //Asserting that the updated value is visible.
     getIssueDetailsModal().within(() => {
       cy.contains("div", "6h logged").should("exist");
+      cy.contains("div", "2h remaining").should("exist");
     });
     //Removing the logged time.
+    cy.get(Stopwatch).click();
     getTimeTrackingModal().within(() => {
+      cy.contains("Time spent (hours)");
       cy.get('[placeholder="Number"]').first().click().clear();
+      cy.contains("Time remaining (hours)");
+      cy.get('[placeholder="Number"]').last().click().clear();
       cy.contains("button", "Done").click().should("not.exist");
     });
     //Asserting that the value is removed.
     getIssueDetailsModal().within(() => {
       cy.contains("div", "No time logged").should("exist");
+      cy.contains("div", "8h estimated").should("be.visible");
     });
   });
 });
